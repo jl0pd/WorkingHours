f7b0e2cd39c70b408d9fab705c25a8b1e10f5173
Denis Proshutinskii <jl0pd@outlook.com>
2020-06-07 21:45:14 +0700

    Added unhandled exception handle

--- WorkingHours/App.xaml.cs
+++ WorkingHours/App.xaml.cs
@@ -1,6 +1,10 @@
-﻿using Avalonia;
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Avalonia.Threading;
 using WorkingHours.ViewModels;
 using WorkingHours.Views;
 
@@ -15,15 +19,29 @@ namespace WorkingHours
 
         public override void OnFrameworkInitializationCompleted()
         {
-            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            var MainWindow = new MainWindow
             {
-                desktop.MainWindow = new MainWindow
-                {
-                    DataContext = new MainWindowViewModel(),
-                };
+                DataContext = new MainWindowViewModel(),
+            };
+
+            switch (ApplicationLifetime)
+            {
+            case IClassicDesktopStyleApplicationLifetime desktop:
+                desktop.MainWindow = MainWindow;
+                break;
+            case ISingleViewApplicationLifetime singleView:
+                singleView.MainView = MainWindow;
+                break;
+            default:
+                throw new NotImplementedException();
             }
 
+            TaskScheduler.UnobservedTaskException += (sender, e) => OnUnhandledExceptionThrown(e.Exception);
+            AppDomain.CurrentDomain.UnhandledException += (sender, e) => OnUnhandledExceptionThrown((Exception)e.ExceptionObject);
+            
             base.OnFrameworkInitializationCompleted();
         }
+
+        private void OnUnhandledExceptionThrown(Exception? e) => Debug.WriteLine(e);
     }
 }
